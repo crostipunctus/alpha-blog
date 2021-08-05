42204ee63c59e021b027c6a15776ebc61b7951ea
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-05 11:42:56 +0530

    restricting users from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     
   end
@@ -18,6 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save 
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.username}, you have succesfully signed up!"
       redirect_to @user
     else
@@ -26,11 +28,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update
-    @user = User.find(params[:id])
+    
 
     if @user.update(user_params)
       redirect_to @user
@@ -46,6 +48,10 @@ class UsersController < ApplicationController
 
   def user_params 
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
