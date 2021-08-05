8ab640750431dbff147336bbe189ec1ec021d4ca
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-05 12:21:13 +0530

    delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -34,16 +34,19 @@ class UsersController < ApplicationController
   end
 
   def update
-    
-
     if @user.update(user_params)
       redirect_to @user
       flash[:notice] = "Updated succesfully"
     else
       render :edit
     end
+  end
 
-
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles succesfully deleted"
+    redirect_to articles_path
   end
 
   private
