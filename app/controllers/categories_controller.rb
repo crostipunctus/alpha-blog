627731f914f7a12b96e9d39b86237c6748e6be86
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-07 11:26:11 +0530

    frt

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+class CategoriesController < ApplicationController
+
+  def index 
+    @categories = Category.all
+  end 
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created succesfully"
+      redirect_to @category
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
